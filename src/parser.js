b6ff583c19fdcb10f4c0dfb7197b091eae66def5
Dario Morandini <dario.morandini@neosperience.com>
2019-10-22 00:53:54 +0200

    Improved parser.

--- src/parser.js
+++ src/parser.js
@@ -2,17 +2,25 @@
 
 const
     { assign } = require('lodash'),
+    fs = require('fs'),
     JSMLHandler = require('./handler.js'),
     htmlparser = require('htmlparser2');
 
 class JSMLParser {
-    parse (xml, options) {
+
+    parseString (xml, options) {
         const handler = new JSMLHandler();
         const parser = new htmlparser.Parser(handler, assign(options || {}, { xmlMode: true, decodeEntities: true }));
         parser.write(xml);
         parser.end();
         return handler.root;
     }
+
+    parseFile (filename, options) {
+        const xml = fs.readFileSync(filename, 'utf8');
+        return this.parseString(xml, options);
+    }
+
 }
 
 module.exports = JSMLParser;
